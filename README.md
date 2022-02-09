030cb278a3139d81a80b4e6470297754d3684a24
Tony Ivanov <telamohn@pm.me>
2022-02-09 10:55:53 +0100

    automated release script, TODO: source tag

---
+++ README.md
@@ -0,0 +1,9 @@
+# Picochat releases
+
+When your app has no servers it can mooch
+hosting off the evil company.
+
+This repository is backing https://pico.pub
+
+Refer to https://github.com/telamon/picochat for source code.
+
---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf ./docs \
+  && mkdir ./docs \
+  && git submodule init src/ \
+  && git submodule update -r src/ \
+  && git submodule foreach 'git pull' \
+  && cd src \
+  && yarn && yarn build \
+  && cd .. \
+  && cp -r src/public/* docs/ \
+  && echo "pico.pub" > docs/CNAME \
+  && touch docs/.nojekyll \
+  && git add . \
+  && git commit -am "automated release script, TODO: source tag" \
+  && echo "done"
+
+
