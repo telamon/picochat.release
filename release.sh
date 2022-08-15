560898099a947e799ee26dbedb1cabad038f715b
Tony Ivanov <telamohn@pm.me>
2022-08-15 20:01:51 +0200

    automated release 7f652cc

--- release.sh
+++ release.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
 rm -rf ./docs \
   && mkdir ./docs \
   && git submodule init src/ \
   && git submodule update -r src/ \
   && git submodule foreach 'git pull' \
   && cd src \
-  && yarn && yarn build \
+  && yarn && yarn test && yarn build \
+  && REV=$(git rev-parse --short HEAD) \
   && cd .. \
   && cp -r src/public/* docs/ \
   && echo "pico.pub" > docs/CNAME \
   && touch docs/.nojekyll \
   && git add . \
-  && git commit -am "automated release script, TODO: source tag" \
-  && echo "done, don't forget to 'git push'"
+  && git commit -am "automated release $REV" \
+  && git push \
+  && echo "done $REV"
 
 
